e333129a3f84fcfce4c8ae7e478b4a3c4659538b
Amanda <allonsmandy@gmail.com>
2020-10-12 03:35:14 -0300

    refactor: equal & notEqual

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -49,10 +49,10 @@ suite('Unit Tests', function(){
     /** 5 - .equal(), .notEqual() **/
     // .equal() compares objects using '=='
     test('#equal, #notEqual', function(){
-      assert.fail( 12, '12', 'numbers are coerced into strings with == ');
-      assert.fail( {value: 1}, {value:1}, '== compares object references');
-      assert.fail( 6 * '2', '12', 'no more hints...');
-      assert.fail( 6 + '2', '12', 'type your error message if you want' );
+      assert.equal( 12, '12', 'numbers are coerced into strings with == ');
+      assert.notEqual( {value: 1}, {value:1}, '== compares object references');
+      assert.equal( 6 * '2', '12', 'no more hints...');
+      assert.notEqual( 6 + '2', '12', 'type your error message if you want' );
     });
     /** 6 - .strictEqual(), .notStrictEqual() **/
     // .strictEqual() compares objects using '==='
