9a8520e7a951875d76d5f6154f0c9d3292035ad3
Amanda <allonsmandy@gmail.com>
2020-10-12 03:50:54 -0300

    refactor: include & notInclude

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -122,8 +122,8 @@ suite('Unit Tests', function(){
     
     /** 12 - #include vs #notInclude **/
     test('Array #include, #notInclude', function() {
-      assert.fail(winterMonths, 'jul', "It's summer in july...");
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language !!');
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
+      assert.include(backendLanguages, 'javascript', 'JS is a backend language !!');
     });
   });
 
