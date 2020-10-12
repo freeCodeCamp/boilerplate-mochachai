e9714e21a56066b1f2cbd0dd78d532888abf0430
Amanda <allonsmandy@gmail.com>
2020-10-12 03:57:25 -0300

    refactor: typeOf & notTypeOf

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -192,11 +192,11 @@ suite('Unit Tests', function(){
       /** 17 #typeOf asserts that value’s type is the given string, **/
       // as determined by Object.prototype.toString.
       // Use #typeOf or #notTypeOf where appropriate
-      assert.fail(myCar, 'object');
-      assert.fail(myCar.model, 'string');
-      assert.fail(airlinePlane.wings, 'string');
-      assert.fail(airlinePlane.engines, 'array');
-      assert.fail(myCar.wheels, 'number');
+      assert.typeOf(myCar, 'object');
+      assert.typeOf(myCar.model, 'string');
+      assert.notTypeOf(airlinePlane.wings, 'string');
+      assert.typeOf(airlinePlane.engines, 'array');
+      assert.typeOf(myCar.wheels, 'number');
     });
 
     test('#instanceOf, #notInstanceOf', function() {
