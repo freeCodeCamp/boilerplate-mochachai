cdbdbc630f68cf01df2744b3ead38f4087ea923a
Amanda <allonsmandy@gmail.com>
2020-10-12 03:23:44 -0300

    refactor: isDefined & isUndefined

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -16,9 +16,9 @@ suite('Unit Tests', function(){
   
     /** 2 - Use assert.isDefined() or assert.isUndefined() to make the tests pass. **/
     test('#isDefined, #isUndefined', function(){
-      assert.fail( null, 'null is not undefined');
-      assert.fail( undefined, 'undefined IS undefined');
-      assert.fail( 'hello', 'a string is not undefined' );
+      assert.isDefined( null, 'null is not undefined');
+      assert.isUndefined( undefined, 'undefined IS undefined');
+      assert.isDefined( 'hello', 'a string is not undefined' );
     });
   
     /** 3 - Use assert.isOk() or assert.isNotOk() to make the tests pass. **/
