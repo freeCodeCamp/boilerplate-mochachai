0a6d22f9df2ebe34cf3838f97d2ccfd30da01533
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-11-06 10:36:31 +0000

    add numbers to func-tests
    Co-authored-by: Randell Dawson <rdawson@onepathtech.com>

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,155 +7,68 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
-  // ### EXAMPLE ###
-  test("Asynchronous test #example", function (done) {
-    setTimeout(function () {
-      assert.isOk("Async test !!");
-      done(); /** Call 'done()' when the async operation is completed**/
-    }, 500); // the function will be executed after 500ms
-  });
-
-  // NOTE: The tests having #example in their description string,
-  // are instructional examples and are not parsed by our test analyser
-
   suite("Integration tests with chai-http", function () {
-    // ### EXAMPLE ###
-    suite('GET /hello?name=[name] => "hello [name]"', function () {
-      // We send a name string in the url query string.
-      test("#example - ?name=John", function (done) {
-        // Don't forget the callback...
-        chai
-          .request(server) // 'server' is the Express App
-          .get("/hello?name=John") // http_method(url)
-          .end(function (err, res) {
-            // Send the request. Pass a callback in
-            // node style. `res` is the response object
-            // res.status contains the status code
-            assert.equal(res.status, 200, "response status should be 200");
-            // res.text contains the response as a string
-            assert.equal(
-              res.text,
-              "hello John",
-              'response should be "hello John"'
-            );
-            done();
-          });
-      });
-
-      test("Test GET /hello with no name", function (done) {
-        chai
-          .request(server)
-          .get("/hello")
-          .end(function (err, res) {
-            assert.fail(res.status, 200);
-            assert.fail(res.text, "hello Guest");
-            done();
-          });
-      });
-
-      test("Test GET /hello with your name", function (done) {
-        chai
-          .request(server)
-          .get("/hello?name=xy_z")
-          .end(function (err, res) {
-            assert.fail(res.status, 200);
-            assert.fail(res.text, "hello xy_z");
-            done();
-          });
-      });
-    });
-
-    // ### EXAMPLE ###
-    suite("PUT /travellers", function () {
-      test('#example - responds with appropriate JSON data when sending {surname: "Polo"}', function (done) {
-        chai
-          .request(server)
-          .put("/travellers") // note the PUT method
-          .send({ surname: "Polo" }) // attach the payload, encoded as JSON
-          .end(function (err, res) {
-            // Send the request. Pass a Node callback
-
-            assert.equal(res.status, 200, "response status should be 200");
-            assert.equal(
-              res.type,
-              "application/json",
-              "Response should be json"
-            );
-
-            // res.body contains the response parsed as a JS object, when appropriate
-            // (i.e the response type is JSON)
-            assert.equal(
-              res.body.name,
-              "Marco",
-              'res.body.name should be "Marco"'
-            );
-            assert.equal(
-              res.body.surname,
-              "Polo",
-              'res.body.surname should be "Polo"'
-            );
-
-            // call 'done()' when... done
-            done();
-          });
-      });
-
-      test('send {surname: "Colombo"}', function (done) {
-        chai
-          .request(server)
-          .put("/travellers")
-
-          .end(function (err, res) {
-            assert.fail();
-
-            done();
-          });
-      });
-
-      test('send {surname: "da Verrazzano"}', function (done) {
-        assert.fail();
-        done();
-      });
-    });
-  });
-
-  const Browser = require("zombie");
-
-  suite("e2e Testing with Zombie.js", function () {
-
-    suite('"Famous Italian Explorers" form', function () {
-      // ### EXAMPLE ###
-      test('#test - submit the input "surname" : "Polo"', function (done) {
-        browser.fill("surname", "Polo").pressButton("submit", function () {
-          // pressButton is ## Async ##.
-          // It waits for the ajax call to complete...
-
-          // assert that status is OK 200
-          browser.assert.success();
-          // assert that the text inside the element 'span#name' is 'Marco'
-          browser.assert.text("span#name", "Marco");
-          // assert that the text inside the element 'span#surname' is 'Polo'
-          browser.assert.text("span#surname", "Polo");
-          // assert that the element(s) 'span#dates' exist and their count is 1
-          browser.assert.element("span#dates", 1);
-
-          done(); // It's an async test, so we have to call 'done()''
-        });
-      });
-
-      test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-        browser.fill("surname", "Colombo").pressButton("submit", function () {
-          assert.fail();
-
+    // #1
+    test("Test GET /hello with no name", function (done) {
+      chai
+        .request(server)
+        .get("/hello")
+        .end(function (err, res) {
+          assert.fail(res.status, 200);
+          assert.fail(res.text, "hello Guest");
           done();
         });
-        //
-      });
+    });
+    // #2
+    test("Test GET /hello with your name", function (done) {
+      chai
+        .request(server)
+        .get("/hello?name=xy_z")
+        .end(function (err, res) {
+          assert.fail(res.status, 200);
+          assert.fail(res.text, "hello xy_z");
+          done();
+        });
+    });
+  });
+  // #3
+  test('send {surname: "Colombo"}', function (done) {
+    chai
+      .request(server)
+      .put("/travellers")
 
-      test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+      .end(function (err, res) {
         assert.fail();
+
         done();
       });
+  });
+  // #4
+  test('send {surname: "da Verrazzano"}', function (done) {
+    assert.fail();
+
+    done();
+  });
+});
+
+const Browser = require("zombie");
+
+suite("e2e Testing with Zombie.js", function () {
+
+  suite('"Famous Italian Explorers" form', function () {
+    // #5
+    test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
+      browser.fill("surname", "Colombo").pressButton("submit", function () {
+        assert.fail();
+
+        done();
+      });
+    });
+    // #6
+    test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
+      assert.fail();
+
+      done();
     });
   });
 });
