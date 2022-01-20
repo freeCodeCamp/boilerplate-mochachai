28f5da69d78c4afddd62515ff3e99acf112c9b15
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-20 10:47:27 +0100

    get port from env if available

--- server.js
+++ server.js
@@ -82,8 +82,9 @@ app.get('/_api/get-tests', cors(), function (req, res, next) {
   });
 
 
-app.listen(3000, function () {
-  console.log("Listening on port " + 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Listening on port " + port);
   console.log('Running Tests...');
   setTimeout(function () {
     try {
