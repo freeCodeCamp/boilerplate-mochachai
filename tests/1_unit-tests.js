e1cc0898cdf49af4f2a87808f0e2779fb094def1
Bong Bungalan <bbungalan@gmail.com>
2022-02-28 11:54:04 +0800

    First Solution

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,10 +33,10 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.notEqual(12 == '12'); //, 'Numbers are coerced into strings with ==');
-      assert.notEqual({ value: 1 } == { value: 1 }, '== compares object references');
-      assert.equal(6 * '2', '12');
-      assert.equal(6 + '2', '12');
+      assert.notEqual(12, '12', 'numbers are coerced into strings with == ');
+      assert.equal({ value: 1 } , { value: 1 }, '== compares object references');
+      assert.notEqual(6 * '2', '12');
+      assert.notEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
