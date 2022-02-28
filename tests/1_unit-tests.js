31135cd944ea036d6304111bea4e99736eba8c38
Bong Bungalan <bbungalan@gmail.com>
2022-02-28 12:02:39 +0800

    'Solved'

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,9 +33,9 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.notEqual(12, '12', 'numbers are coerced into strings with == ');
-      assert.equal({ value: 1 } , { value: 1 }, '== compares object references');
-      assert.notEqual(6 * '2', '12');
+      assert.equal(12, '12', 'numbers are coerced into strings with == ');
+      assert.notEqual({ value: 1 } , { value: 1 }, '== compares object references');
+      assert.equal(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
     });
     // #6
