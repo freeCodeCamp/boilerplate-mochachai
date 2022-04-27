c9e474d0d42a4c42549dcb20fa737b001c65116d
DJMDNOW <104536083+DJMDNOW@users.noreply.github.com>
2022-04-27 19:31:11 -0400

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.isNull(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull(1, '1 is not null');
+      assert.isNotNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
