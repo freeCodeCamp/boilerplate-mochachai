0fefdcbb2f3cff4a7e09f65c800dbd9d757b83cf
Consuelo Gómez <mconsuelo.gomezt@gmail.com>
2023-06-09 16:17:35 -0400

    Test if a Value is an Array

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -87,8 +87,8 @@ suite('Unit Tests', function () {
   suite('Arrays', function () {
     // #11
     test('#isArray, #isNotArray', function () {
-      assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
-      assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number');
+      assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
+      assert.isNotArray([1, 2, 3].indexOf(2), 'indexOf returns a number');
     });
     // #12
     test('Array #include, #notInclude', function () {
