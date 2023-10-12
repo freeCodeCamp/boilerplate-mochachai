4ba1c552805c49b0ac8e152006eed1efac89a609
Namrata Shirsat <123083760+Namrata-1487@users.noreply.github.com>
2023-10-12 19:40:29 +0530

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,23 +6,23 @@ suite('Unit Tests', function () {
     // #1
     test('#isNull, #isNotNull', function () {
       assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isTrue(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
       assert.fail(null, 'null is not undefined');
       assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+      assert.isTrue('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
       assert.fail(null, 'null is falsey');
       assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isTrue(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
+      assert.isTrue(true, 'true is true');
       assert.fail(!!'double negation', 'Double negation of a truthy value is true');
       assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
