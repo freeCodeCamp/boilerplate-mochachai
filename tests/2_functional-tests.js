509d784412b6f6017f129b834d34b3ed5c9d34c3
chrvistian <150487346+chrvistian@users.noreply.github.com>
2023-11-16 12:21:19 -0600

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -48,11 +48,20 @@ suite('Functional Tests', function () {
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      /** place the chai-http request code here... **/
+      chai.request(server)
+          .put('/travellers')
+          .send({ surname: 'da Verrazzano', name: 'Giovanni' })
+        
+        /** place your tests inside the callback **/
+        .end(function(_err, res) {
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
+          done();
+        });
     });
-  });
 });
 
 const Browser = require('zombie');
