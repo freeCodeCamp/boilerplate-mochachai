273653ffdeafeea1e7c0800d3538c2490fe834cd
Mattheus Luiz <matth.luiz10@gmail.com>
2023-12-10 13:33:53 -0300

    Questao 01 e 02

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,20 +5,20 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.fail(1, '1 is not null');
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.fail(null, 'null is not undefined');
-      assert.fail(undefined, 'undefined IS undefined');
-      assert.fail('hello', 'A string is not undefined');
+      assert.isNull(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
-      assert.fail(null, 'null is falsey');
-      assert.fail("I'm truthy", 'A string is truthy');
-      assert.fail(true, 'true is truthy');
+      assert.isNull(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isNotOk(true, 'true is truthy');
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
