6b078fa5fe5cfadf7b7e5748a77ce241f9ed5003
Taurai J Munodawafa <taujob1111@gmail.com>
2024-02-15 13:24:49 +0000

    Using isTrue/isNotTrue Tes1t

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -24,8 +24,8 @@ test('#isOk, #isNotOk', function() {
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.isTrue(true, 'true is true');
-      assert.isNotTrue(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
