f2da551fe93f7996b57f9a7c8cadcf1e8791d8e7
jebrodock <164965318+jebrodock@users.noreply.github.com>
2024-03-26 10:10:30 +0900

    Update 1_unit-tests.js
    Changed 13 to isString and isNotString

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -105,9 +105,9 @@ suite('Unit Tests', function () {
   suite('Strings', function () {
     // #13
     test('#isString, #isNotString', function () {
-      assert.fail(Math.sin(Math.PI / 4), 'A float is not a string');
-      assert.fail(process.env.PATH, 'An env variable is a string (or undefined)');
-      assert.fail(JSON.stringify({ type: 'object' }), 'JSON is a string');
+      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
+      assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
+      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
     // #14
     test('String #include, #notInclude', function () {
