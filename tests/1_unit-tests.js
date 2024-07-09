c115f2d49c1fef252013a8303169c9b4401f026b
Hario Blackid <hario0079.hs@gmail.com>
2024-07-09 05:00:13 +0000

    Change #1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,8 +4,8 @@ const assert = chai.assert;
 suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
-    test('isNull, isNotNull', function () {
-      assert.fail(null, 'This is an optional error description - e.g. null is null');
+    test('#isNull, #isNotNull', function () {
+      assert.isNull(null, 'This is an optional error description - e.g. null is null');
       assert.fail(1, '1 is not null');
     });
     // #2
