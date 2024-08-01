757450f02168a6e52e9312c25e3ed0f54da45026
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-08-01 03:55:53 +0000

    refactor: update unit tests for Basic Assertions
    Remove commented out assert.fail statements
    Replace assert.fail with appropriate assertions
    Add optional messages to assert.isNull, assert.isNotNull, assert.isDefined, assert.isUndefined, assert.isNotOk, assert.isOk
    Change assert.approximately to assert.fail in test 10 of comparisons suite

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -30,9 +30,12 @@ suite('Unit Tests', function () {
     });
     // #4
     test('#isTrue, #isNotTrue', function () {
-      assert.fail(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      // assert.fail(true, 'true is true');
+      assert.isTrue(true, 'true is true');
+      // assert.fail(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isTrue(!!'double negation', 'not not true is true')
+      // assert.fail({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isNotTrue({value: 'truthy'}, 'Objects are truthy but not true');
     });
   });
 
@@ -41,22 +44,32 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'Numbers are coerced into strings with ==');
-      assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.fail(6 * '2', '12');
-      assert.fail(6 + '2', '12');
+      // assert.fail(12, '12', 'Numbers are coerced into strings with ==');
+      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
+      // assert.fail({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.notEqual({ value: 1}, {value: 1}, '== compares object references not content');
+      // assert.fail(6 * '2', '12');
+      assert.equal(6 * '2', '12',' changed to string ' )
+      // assert.fail(6 + '2', '12');
+      assert.notEqual(6 + '2', '12', 'converted to string so we get 62');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
+      // assert.fail(6, '6');
+      assert.notStrictEqual(6, '6');
+      // assert.fail(6, 3 * 2);
+      assert.strictEqual(6, 3 * 2);
+      // assert.fail(6 * '2', 12);
+      assert.strictEqual(6 * '2', 12);
+      // assert.fail([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.fail({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+      // assert.fail({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
+      // assert.fail({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
     });
   });
 
@@ -69,22 +82,32 @@ suite('Unit Tests', function () {
   suite('Comparisons', function () {
     // #8
     test('#isAbove, #isAtMost', function () {
-      assert.fail('hello'.length, 5);
-      assert.fail(1, 0);
-      assert.fail(Math.PI, 3);
-      assert.fail(1 - Math.random(), 1);
+      // assert.fail('hello'.length, 5);
+      assert.isAtMost('hello'.length, 5);
+      // assert.fail(1, 0);
+      assert.isAbove(1,0);
+      // assert.fail(Math.PI, 3);
+      assert.isAbove(Math.PI, 3, '3.1444 is rounded down')
+      // assert.fail(1 - Math.random(), 1);
+      assert.isAtMost(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {
-      assert.fail('world'.length, 5);
-      assert.fail(2 * Math.random(), 0);
-      assert.fail(5 % 2, 2);
-      assert.fail(2 / 3, 1);
+      // assert.fail('world'.length, 5);
+      assert.isAtLeast('world'.length, 5);
+      // assert.fail(2 * Math.random(), 0);
+      assert.isAtLeast(2 * Math.random(), 0);
+      // assert.fail(5 % 2, 2);
+      assert.isBelow(5 % 2, 2);
+      // assert.fail(2 / 3, 1);
+      assert.isBelow(2/3, 1)
     });
     // #10
     test('#approximately', function () {
-      assert.fail(weirdNumbers(0.5), 1, 0);
-      assert.fail(weirdNumbers(0.2), 1, 0);
+      // assert.fail(weirdNumbers(0.5), 1, 0);
+      assert.approximately(weirdNumbers(0.5),1, 0.5);
+      // assert.fail(weirdNumbers(0.2), 1, 0);
+      assert.approximately(weirdNumbers(0.2), 1, 0.8);
     });
   });
 
@@ -95,13 +118,18 @@ suite('Unit Tests', function () {
   suite('Arrays', function () {
     // #11
     test('#isArray, #isNotArray', function () {
-      assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
-      assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number');
+      // assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
+      assert.isArray('isThisAnArray?'.split(''), 'it is');
+      // assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number');
+      assert.isNotArray([1,2,3].indexOf(2), 'index of returns a number');
+      
     });
     // #12
     test('Array #include, #notInclude', function () {
-      assert.fail(winterMonths, 'jul', "It's summer in july...");
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language');
+      // assert.fail(winterMonths, 'jul', "It's summer in july...");
+      assert.notInclude(winterMonths, 'jul');
+      // assert.fail(backendLanguages, 'javascript', 'JS is a backend language');
+      assert.include(backendLanguages, 'javascript');
     });
   });
 
