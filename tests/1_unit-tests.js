a4dece773060304d6eb0b53a04a60b4cba4a2afa
Phillip Kim <phillip.kim@boston.gov>
2024-09-24 10:55:26 -0400

    fix asserts

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,8 +5,8 @@ suite('Unit Tests', function () {
   suite('Basic Assertions', function () {
     // #1
     test('#isNull, #isNotNull', function () {
-      assert.isNull.fail(null, 'This is an optional error description - e.g. null is null');
-      assert.isNotNull.fail(1, '1 is not null');
+      assert.isNull;
+      assert.isNotNull;
     });
     // #2
     test('#isDefined, #isUndefined', function () {
