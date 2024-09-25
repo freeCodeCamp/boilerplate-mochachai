7f294e34604e48b5ff3a69981c8ee7b25b921dab
FoxAllUp <richardhanzel93@gmail.com>
2024-09-25 13:04:11 +0200

    Lesson6

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -42,8 +42,8 @@ suite('Unit Tests', function () {
     test('#strictEqual, #notStrictEqual', function () {
       assert.notStrictEqual(6, '6');
       assert.strictEqual(6, 3 * 2);
-      assert.notStrictEqual(6 * '2', 12);
-      assert.strictEqual([1, 'a', {}], [1, 'a', {}]);
+      assert.strictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
