2e173de95a848cdca9a3a958f4cf710270069bdb
Richárd Hanzel <richardhanzel93@gmail.com>
2024-09-25 10:59:00 +0000

    Lesson5

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -34,7 +34,7 @@ suite('Unit Tests', function () {
     // #5
     test('#equal, #notEqual', function () {
       assert.equal(12, '12', 'Numbers are coerced into strings with ==');
-      assert.equal({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
       assert.equal(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
     });
