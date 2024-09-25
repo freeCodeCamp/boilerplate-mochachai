454f2cb1c95c47e78460506455f90385ea36f735
FoxAllUp <richardhanzel93@gmail.com>
2024-09-25 13:11:12 +0200

    lesson7

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -48,7 +48,7 @@ suite('Unit Tests', function () {
     // #7
     test('#deepEqual, #notDeepEqual', function () {
       assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
-      assert.deepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
     });
   });
 
