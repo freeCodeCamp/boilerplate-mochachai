c3d6a241db9e7ef8b3395db749902569349bb604
Richárd Hanzel <richardhanzel93@gmail.com>
2024-09-25 10:53:01 +0000

    lesson4

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -23,7 +23,7 @@ suite('Unit Tests', function () {
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.isTrue(true, 'true is true');
-      assert.isNotTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
       assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
